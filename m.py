614c6fb3a0c5842a190ab823a04fe13ee9f97136
moshdash <130574600+moshdash@users.noreply.github.com>
2023-05-06 11:41:31 +0100

    refining results

--- m.py
+++ m.py
@@ -3,6 +3,7 @@
 import requests
 import json
 from pprint import pprint as pp
+import ast
 
 url = "https://www.dnd5eapi.co/api/ability-scores"
 r = requests.get(url)
@@ -15,325 +16,302 @@ class DND:
     def getAbilityScores(self, name = None):
         baseurl = self.apiurl + "ability-scores"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
             
     
     def getAlignments(self, name = None):
         baseurl = self.apiurl + "alignments"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getBackgrounds(self, name = None):
         baseurl = self.apiurl + "backgrounds"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getClasses(self, name = None):
         baseurl = self.apiurl + "classes"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getDamageTypes(self, name = None):
         baseurl = self.apiurl + "damage-type"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getEquipment(self, name = None):
         baseurl = self.apiurl + "equipment"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getEquipmentCategories(self, name = None):
         baseurl = self.apiurl + "equipment-categories"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
 
     def getFeats(self, name = None):
         baseurl = self.apiurl + "feats"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getFeatures(self, name = None):
         baseurl = self.apiurl + "features"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getLanguages(self, name = None):
         baseurl = self.apiurl + "languages"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getMagicItems(self, name = None):
         baseurl = self.apiurl + "magic-items"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getMagicSchools(self, name = None):
         baseurl = self.apiurl + "magic-schools"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
         
     def getMonsters(self, name = None):
         baseurl = self.apiurl + "monsters"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getProficiencies(self, name = None):
         baseurl = self.apiurl + "proficiencies"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
 
     def getRaces(self, name = None):
         baseurl = self.apiurl + "races"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getRuleSections(self, name = None):
         baseurl = self.apiurl + "rule-sections"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getRules(self, name = None):
         baseurl = self.apiurl + "rules"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getSkills(self, name = None):
         baseurl = self.apiurl + "skills"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getSpells(self, name = None):
         baseurl = self.apiurl + "spells"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
 
     def getSubclasses(self, name = None):
         baseurl = self.apiurl + "subclasses"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getSubraces(self, name = None):
         baseurl = self.apiurl + "subraces"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getTraits(self, name = None):
-        baseurl = self.apiurl + "Traits"
+        baseurl = self.apiurl + "traits"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
     
     def getWeaponProperties(self, name = None):
         baseurl = self.apiurl + "weapon-properties"
         if name == None:
-            print(baseurl)
             r = requests.get(baseurl)
             data =r.json()
-            return data
+            results = data['results']
+            return results
         else:
             nameurl = (f"{baseurl}/{name}")
             r = requests.get(nameurl)
             data =r.json()
-            print(nameurl)
             return data
 dnd = DND()
 
-pp(dnd.getSkills("stealth"))
+pp(dnd.getAbilityScores('cha'))
